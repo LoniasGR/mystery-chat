e830a2d72408bd33c38b77510859e7db5c4b0658
Leonidas Avdelas <avdelasleonidas@gmail.com>
2024-11-09 14:25:24 +0200

    start WebSockets

--- mystery-backend/index.ts
+++ mystery-backend/index.ts
@@ -1,13 +1,19 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import { createServer } from "http";
 import path from "path";
+import { Server } from "socket.io";
 import { client, connectOrExit, envOrDefault, morgan } from "./src/configs";
 import { authController, userController } from "./src/controllers";
 import { users } from "./src/data/users.json";
 import { UserService } from "./src/services/UserService";
 
-// Set up middleware
+// Set up the servers
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+// Set up middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan);
@@ -29,7 +35,17 @@ await UserService.createUsers(users);
 app.use("/users", userController);
 app.use("/auth", authController);
 
+io.on("connection", async (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
 const port = envOrDefault("APP_PORT");
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
