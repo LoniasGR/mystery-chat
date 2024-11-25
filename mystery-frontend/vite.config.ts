53ec1bbd011915fd1fdd4b6d5db1d4ed2781c752
Oskar Pawica <pawicao@gmail.com>
2024-11-25 01:40:06 +0100

    PWA

--- mystery-frontend/vite.config.ts
+++ mystery-frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import path from "path";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), VitePWA()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
